2e0282aff5a40844bdc5c6841bb6d28fac7a4f7a
fahad484 <mdf422008@gmail.com>
2026-08-01 18:05:07 +0530

    "first commit"

--- app.js
+++ app.js
@@ -67,9 +67,9 @@ const sessionOptions = {
         httpOnly : true,
     }
 }
-// app.get("/",(req,res)=>{
-//     res.send('the root is working properly');
-// });
+app.get("/",(req,res)=>{
+    res.redirect("/listings");
+});
 
 app.use(session(sessionOptions));
 app.use(flash());
